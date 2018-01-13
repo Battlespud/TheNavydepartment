f5a9e0984f57bd5bd568da3414d65956b900f763
Battlespud <battlespud@hotmail.com>
2018-01-12 21:30:15 -0500

    SmoothScroll

---
+++ Assets/CenterTrackTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterTrackTrigger : MonoBehaviour {
+
+	
+	public Camera Cam;
+	public Rigidbody CRb;
+	private PositionRefresher pos;
+	
+	public GameObject Player;
+	
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag("Player");
+		Cam = Camera.main;
+		CRb = Cam.GetComponent<Rigidbody>();
+		pos = Cam.GetComponent<PositionRefresher>();
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject == Player)
+			pos.RecieveInput = false;
+		//		CRb.isKinematic = true;
+	}
+	
+	
+	private void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject == Player)
+			pos.RecieveInput = true;
+
+		//	CRb.isKinematic = false;
+	}
+}
---
+++ Assets/TestMovementScene/MovementController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Facing
+{
+	LEFT,
+	RIGHT
+}
+
+public class MovementController : MonoBehaviour
+{
+
+	public CharacterController Controller;
+	public Facing facing = Facing.RIGHT;
+
+	public Sprite Right;
+	public Sprite Left;
+
+	Dictionary<Facing,Sprite> Sprites = new Dictionary<Facing, Sprite>();
+	
+	public SpriteRenderer Renderer;
+	
+	private static float BaseSpeed = 5f;
+
+	public static float GetSpeed()
+	{
+		return BaseSpeed;
+	}
+	
+	void Awake()
+	{
+		Controller = GetComponent<CharacterController>();
+		Renderer = GetComponentInChildren<SpriteRenderer>();
+		Sprites.Add(Facing.RIGHT, Right);
+		Sprites.Add(Facing.LEFT, Left);
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		InputProcess();
+	}
+
+	
+	void InputProcess()
+	{
+		Vector2 Movement = new Vector2();
+		if (Input.GetKey(KeyCode.W))
+			Movement.y += BaseSpeed;
+		if(Input.GetKey(KeyCode.A))
+			Movement.x -= BaseSpeed;
+		if (Input.GetKey(KeyCode.S))
+			Movement.y -= BaseSpeed;
+		if(Input.GetKey(KeyCode.D))
+			Movement.x += BaseSpeed;
+		Move(Movement);
+	}
+	
+
+	void Move(Vector2 M)
+	{
+		if (M.x < 0)
+			facing = Facing.LEFT;
+		else if (M.x > 0)
+			facing = Facing.RIGHT;
+		Controller.SimpleMove(new Vector3(M.x, 0f, M.y));
+		Renderer.sprite = Sprites[facing];
+	}
+}
